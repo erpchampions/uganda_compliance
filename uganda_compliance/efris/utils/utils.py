7ef6cd55caae82ce1b336f1dc9967e59c8611b33
maniamartial <martialmania19@gmail.com>
2025-04-16 09:11:42 +0300

    chore - removed commented code

--- uganda_compliance/efris/utils/utils.py
+++ uganda_compliance/efris/utils/utils.py
@@ -113,38 +113,3 @@ class BytesArrayEncoder:
     def write(self, b):
         self.byte_list.append(b)
         
-# @frappe.whitelist()
-# def get_qr_code(data: str) -> str:
-#     """Generate QR Code data
-
-#     Args:
-#         data (str): The information used to generate the QR Code
-
-#     Returns:
-#         str: The QR Code.
-#     """
-#     qr_code_bytes = get_qr_code_bytes(data, format="PNG")
-#     base_64_string = bytes_to_base64_string(qr_code_bytes)
-
-#     return add_file_info(base_64_string)
-
-
-# def add_file_info(data: str) -> str:
-#     """Add info about the file type and encoding.
-
-#     This is required so the browser can make sense of the data."""
-#     return f"data:image/png;base64, {data}"
-
-# def get_qr_code_bytes(data: bytes | str, format: str = "PNG") -> bytes:
-#     """Create a QR code and return the bytes."""
-#     img = qrcode.make(data)
-
-#     buffered = BytesIO()
-#     img.save(buffered, format=format)
-
-#     return buffered.getvalue()
-
-
-# def bytes_to_base64_string(data: bytes) -> str:
-#     """Convert bytes to a base64 encoded string."""
-#     return b64encode(data).decode("utf-8")
